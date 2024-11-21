566da7baa21d7fbe3c02d8cebd11a729e77316dd
Tiburce Kouagou <kouagoutiburce@gmail.com>
2024-11-21 17:55:52 +0100

    ajout de model

--- src/Controllers/TodoController.php
+++ src/Controllers/TodoController.php
@@ -2,18 +2,19 @@
 namespace App\Controllers;
 
 use DB\Database;
+use App\Models\Todo;
 
 class TodoController
 {
+    private Todo $todoModel;
+
+    public function __construct() {
+        $this->todoModel = new Todo();
+    }
 
     public function index()
     {
-        // Récupérer l'instance de connexion à la BDD
-        $db = Database::getInstance();
-
-        // Récupérer les tâches depuis la BDD
-        $query = $db->query("SELECT * FROM todos;"); // prépare la requête
-        $todos = $query->fetchAll(); // retourne le résultat de l'exécution de la requête
+        $todos = $this->todoModel->getAll();
 
         // Charger la Vue "Views/index.php"
         // require __DIR__ . "/../Views/index.php";
@@ -26,18 +27,7 @@ class TodoController
             $task = trim($_POST['task']);
 
             if ($task) {
-                // Récupérer l'instance de connexion à la BDD
-                $db = Database::getInstance();
-                // Prépare la requête SQL pour insérer une nouvelle tâche dans la table "todos".
-                // Les placeholders `:task` et `:done` sont utilisés pour éviter les injections SQL.
-                // Cela sécurise les données entrés par l'utilisateur.
-                $stmt = $db->prepare("INSERT INTO todos (task, done) VALUES (:task, :done);");
-
-                // Exécute la requête préparée avec les valeurs spécifiques fournies dans un tableau associatif
-                // - `:task` contient la description de la tâche saisie par l'utilisateur
-                // - `:done` est initialisé à 0 (indiquand que la tâche n'est pas encore terminée).
-                $stmt->execute([":task" => $task, ":done" => 0]);
-                // $stmt->execute(["task" => $task, "done" => 0]); // on peut retirer les ":" des placeholders. C'est pareil !
+                $this->todoModel->add($task);
             }
 
             header('Location: /');
@@ -52,10 +42,7 @@ class TodoController
     {
         $id = $_GET['id'] ?? null;
         if ($id) {
-            // Récupérer l'instance de connexion à la BDD
-            $db = Database::getInstance();
-            $stmt = $db->prepare("DELETE FROM todos WHERE id = :id;"); // $stmt pour "prepared statement" en anglais, "requête préparé" en fr donc stmt ==> statement
-            $stmt->execute(["id" => (int) $id]);
+            $this->todoModel->delete((int) $id);
         }
 
         header('Location: /');
@@ -66,10 +53,7 @@ class TodoController
     {
         $id = $_GET['id'] ?? null;
         if ($id) {
-            // Récupérer l'instance de connexion à la BDD
-            $db = Database::getInstance();
-            $stmt = $db->prepare("UPDATE todos SET done = NOT done WHERE id = :id");
-            $stmt->execute(["id" => (int) $id]);
+            $this->todoModel->toggle((int) $id);
         }
 
         header('Location: /');
---
+++ src/Models/Todo.php
@@ -0,0 +1,68 @@
+<?php
+namespace App\Models;
+
+use DB\Database;
+
+class Todo
+{
+    /**
+     * Récupère toutes les tâches dans la BDD
+     * @return array
+     */
+    public function getAll()
+    {
+        // Récupérer l'instance de connexion à la BDD
+        $db = Database::getInstance();
+
+        // Récupérer les tâches depuis la BDD
+        $query = $db->query("SELECT * FROM todos;"); // prépare la requête
+        return $query->fetchAll(); // retourne le résultat de l'exécution de la requête
+    }
+
+    /**
+     * Ajoute une nouvelle tâche dans la BDD
+     * @param string $task La tâche à ajouter
+     * @return bool
+     */
+    public function add(string $task)
+    {
+        // Récupérer l'instance de connexion à la BDD
+        $db = Database::getInstance();
+        // Prépare la requête SQL pour insérer une nouvelle tâche dans la table "todos".
+        // Les placeholders `:task` et `:done` sont utilisés pour éviter les injections SQL.
+        // Cela sécurise les données entrés par l'utilisateur.
+        $stmt = $db->prepare("INSERT INTO todos (task, done) VALUES (:task, :done);");
+
+        // Exécute la requête préparée avec les valeurs spécifiques fournies dans un tableau associatif
+        // - `:task` contient la description de la tâche saisie par l'utilisateur
+        // - `:done` est initialisé à 0 (indiquand que la tâche n'est pas encore terminée).
+        return $stmt->execute([":task" => $task, ":done" => 0]);
+        // $stmt->execute(["task" => $task, "done" => 0]); // on peut retirer les ":" des placeholders. C'est pareil !
+    }
+
+    /**
+     * Change le status d'une tâche (comme terminée|pas terminée)
+     * @param int $id L'identifiant de la tâche à supprimer
+     * @return bool
+     */
+    public function toggle(int $id)
+    {
+         // Récupérer l'instance de connexion à la BDD
+         $db = Database::getInstance();
+         $stmt = $db->prepare("UPDATE todos SET done = NOT done WHERE id = :id");
+         return $stmt->execute(["id" => (int) $id]);
+    }
+
+    /**
+     * Supprime une tâche
+     * @param int $id L'identifiant de la tâche à supprimer
+     * @return bool
+     */
+    public function delete(int $id)
+    {
+        // Récupérer l'instance de connexion à la BDD
+        $db = Database::getInstance();
+        $stmt = $db->prepare("DELETE FROM todos WHERE id = :id;"); // $stmt pour "prepared statement" en anglais, "requête préparé" en fr donc stmt ==> statement
+        return $stmt->execute(["id" => (int) $id]);
+    }
+}
